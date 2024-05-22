b47dbd0a722cdf0ca7e300f30882183c71509cc7
Eric345343434 <je3301@mci4me.at>
2024-05-22 11:58:20 +0200

    update

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pd
+import plotly.express as px
+import streamlit as st
 
 column_names = ["Duration","Distance","OriginalPace","HeartRate","Cadence","PowerOriginal","CalculatedPace","CalculatedStrideLength","CalculatedAerobicEfficiencyPace","CalculatedAerobicEfficiencyPower","CalculatedEfficiencyIndex"]
 data= pd.read_csv("activity.csv")
 print(data)
+Power_mean= data["PowerOriginal"].mean()
+power_max= data["PowerOriginal"].max()
+fig = px.line(data["PowerOriginal"])
+fig.update_layout(xaxis_title="Time", yaxis_title="Power (Watts)")
+fig2 = px.line(data["HeartRate"])
+fig.update_layout(xaxis_title="Time (s)", yaxis_title="Power (Watt), Hertrate(Hz)")
+fig.add_traces(fig2.data)
+fig.update_traces(line=dict(color=["aliceblue"]*2))
+fig.show()
