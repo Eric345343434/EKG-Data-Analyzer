ba6c4cded481798953340c214186b36034496ca3
Eric345343434 <je3301@mci4me.at>
2024-05-27 09:31:57 +0200

    Einbau der Bilder und Personen-Auswahl

--- Startseite.py
+++ Startseite.py
@@ -1,4 +1,39 @@
 import streamlit as st
+from function import get_person_data, get_person_names, find_person_data_by_name
+from PIL import Image
 
 st.title("Wilkommen bei  unserer Startseite")
 
+person_data = get_person_data()
+person_names_list = get_person_names(person_data)
+print(person_names_list)
+
+# Eine Überschrift der ersten Ebene
+st.write("# EKG APP")
+if "current-user" not in st.session_state:
+    st.session_state.current_user ="None"
+
+# Eine Überschrift der zweiten Ebene
+st.write("## Versuchsperson auswählen")
+
+# Eine Auswahlbox, das Ergebnis wird in current_user gespeichert
+st.session_state.current_user = st.selectbox(
+    'Versuchsperson',
+    options = person_names_list, key="sbVersuchsperson")
+
+st.write(st.session_state.current_user,"wird zurzeit gewählt")
+current_user_list = find_person_data_by_name(st.session_state.current_user) 
+
+
+
+# Anlegen des Session State. Bild, wenn es kein Bild gibt
+if 'picture_path' not in st.session_state:
+    st.session_state.picture_path = 'data/pictures/none.jpg'
+
+
+
+# Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
+if st.session_state.current_user in person_names_list:
+    image = Image.open(current_user_list["picture_path"])
+# Anzeigen eines Bilds mit Caption
+    st.image(image, caption=st.session_state.current_user)
---
+++ function.py
@@ -0,0 +1,61 @@
+def hold_power(values, times, power_lv):
+    held_time = []
+    start= 0
+    for value,time in zip(values,times):
+        if value <  power_lv:
+            start = time
+        if value > power_lv:
+            end = time
+            held_time.append(end-start)
+    return max(held_time) 
+
+value =[1,2,3,4,5,6,7,8]
+time = [1,2,3,4,5,6,7,8]
+power_lv = 2
+
+print(hold_power(value,time,power_lv))
+#%%
+
+import json
+
+def get_person_data():
+    """A Function that knows where te person Database is and returns a Dictionary with the Persons"""
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    return person_data
+
+def get_person_names(person_data):
+    """A Function that takes the persons-dictionary and returns a list auf all person names"""
+    list_of_names = []
+
+    for eintrag in person_data:
+        list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
+    return list_of_names
+
+def find_person_data_by_name(suchstring):
+    """ Eine Funktion der Nachname, Vorname als ein String übergeben wird
+    und die die Person als Dictionary zurück gibt"""
+    
+    person_data = get_person_data()
+    #print(suchstring)
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(", ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        print(eintrag)
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            print()
+
+            return eintrag
+    else:
+        return {}
+
+if __name__ == "__main__":
+ person_names_list = get_person_names()
+ print(person_names_list)
+ suchstring = "Huber, Julian"
+ print(find_person_data_by_name(suchstring))
