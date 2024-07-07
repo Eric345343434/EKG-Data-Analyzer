e9f66a9b4386be725251a6996bb8241321481511
Eric345343434 <je3301@mci4me.at>
2024-07-07 19:59:19 +0200

    Startseite-Logo

--- 1_👋_Startseite.py
+++ 1_👋_Startseite.py
@@ -6,8 +6,15 @@ import matplotlib.pyplot as plt
 
 
 # Eine Überschrift der ersten Ebene
-st.title("Wilkommen bei  unserer Startseite") 
-st.write("# EKG APP")
+st.title("EKG-Data-Analyzer") 
+st.write("Willkommen auf der Startseite ")
+
+
+# Load the logo image
+logo = Image.open('data/pictures/logo.jpg')
+
+# Display the logo
+st.image(logo, caption='', use_column_width=True)
 
 
 
--- pages/2_👤_Personenauswahl.py
+++ pages/2_👤_Personenauswahl.py
@@ -2,6 +2,7 @@ import streamlit as st
 from function import person,ekgdata
 from PIL import Image
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
@@ -38,13 +39,17 @@ st.session_state.current_user = st.selectbox(
 st.session_state.current_user_list = person.find_person_data_by_name(st.session_state.current_user) 
 st.session_state.current_user_id = int(person.find_person_id_by_name(st.session_state.current_user))
 
+col1,col2 =st.columns(2)
 
-# Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
-if st.session_state.current_user in person_names_list:
-    image = Image.open(st.session_state.current_user_list['picture_path'])
-# Anzeigen eines Bilds mit Caption
-    st.image(image, caption=st.session_state.current_user)
+with col1:
 
 
-st.write("Geburtsjahr =",  st.session_state.current_user_list["year_of_birth"])
-st.write(st.session_state.current_user,"wird zurzeit gewählt")
+    # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
+    if st.session_state.current_user in person_names_list:
+        image = Image.open(st.session_state.current_user_list['picture_path'])
+    # Anzeigen eines Bilds mit Caption
+        st.image(image, caption=st.session_state.current_user)
+
+with col2:
+    st.write("Geburtsjahr =",  st.session_state.current_user_list["year_of_birth"])
+    st.write(st.session_state.current_user,"wird zurzeit gewählt")
