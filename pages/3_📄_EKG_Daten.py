f92a4116e599dd5efe471e647c01e7ef0b413fb5
Eric345343434 <163291225+Eric345343434@users.noreply.github.com>
2024-07-07 22:33:13 +0200

    Update 3_📄_EKG_Daten.py

--- pages/3_📄_EKG_Daten.py
+++ pages/3_📄_EKG_Daten.py
@@ -2,43 +2,43 @@ import streamlit as st
 from function import person,ekgdata
 from PIL import Image
 import matplotlib.pyplot as plt
-    st.session_state.current_ekg_list_id= ekgdata.get_ekg_ids_by_person_id(st.session_state.current_user_id)
-    current_ekg_list =[]
-    for i in range(0,len(st.session_state.current_ekg_list_id)):
-        current_ekg_list.append(ekgdata.load_by_ekg_id(st.session_state.current_ekg_list_id[i]))
-    st.session_state.current_ekg_list = current_ekg_list
-    ekg_data= ekgdata.load_ekg_table()
+st.session_state.current_ekg_list_id= ekgdata.get_ekg_ids_by_person_id(st.session_state.current_user_id)
+current_ekg_list =[]
+for i in range(0,len(st.session_state.current_ekg_list_id)):
+    current_ekg_list.append(ekgdata.load_by_ekg_id(st.session_state.current_ekg_list_id[i]))
+st.session_state.current_ekg_list = current_ekg_list
+ekg_data= ekgdata.load_ekg_table()
 
-    st.write("## Ekg-Test auswählen") 
-
-
-    st.session_state.current_ekg_id = st.selectbox(
-        "",
-        options=st.session_state.current_ekg_list_id,
-        key="ekg_tests"
-    )   
-
-    if "current_ekg_id" not in st.session_state:
-        st.session_state.id = "None"
+st.write("## Ekg-Test auswählen") 
 
 
+st.session_state.current_ekg_id = st.selectbox(
+    "",
+    options=st.session_state.current_ekg_list_id,
+    key="ekg_tests"
+)   
 
-
-    st.write("Derzeit ist der Ekg_Test mit der ID", st.session_state.current_ekg_id ,"von", st.session_state.current_user,"ausgewählt")
+if "current_ekg_id" not in st.session_state:
+    st.session_state.id = "None"
 
 
 
 
-    ekg_dict = ekg_data.get(doc_id=str(st.session_state.current_ekg_id))
-    st.write("Datum des Ekg-Tests =",ekg_dict["date"])
+st.write("Derzeit ist der Ekg_Test mit der ID", st.session_state.current_ekg_id ,"von", st.session_state.current_user,"ausgewählt")
 
 
-    ekgdata1= ekgdata(st.session_state.current_ekg_id)
-    st.write("Dauer des Ekg-Tests =",ekgdata1.calc_duration()*1000, "Millisekunden = ", ekgdata1.calc_duration(), "Sekunden = ", ekgdata1.calc_duration()/60, "Minuten")
-    peaks = ekgdata1.find_peaks()
-    st.write("Heartrate:", ekgdata.estimate_hr(peaks))
-    st.title("EKG-Daten konnten nicht geladen werden!")
-    st.header("Versuchen sie zuerst die Person auszuwählen")
+
+
+ekg_dict = ekg_data.get(doc_id=str(st.session_state.current_ekg_id))
+st.write("Datum des Ekg-Tests =",ekg_dict["date"])
+
+
+ekgdata1= ekgdata(st.session_state.current_ekg_id)
+st.write("Dauer des Ekg-Tests =",ekgdata1.calc_duration()*1000, "Millisekunden = ", ekgdata1.calc_duration(), "Sekunden = ", ekgdata1.calc_duration()/60, "Minuten")
+peaks = ekgdata1.find_peaks()
+st.write("Heartrate:", ekgdata.estimate_hr(peaks))
+st.title("EKG-Daten konnten nicht geladen werden!")
+st.header("Versuchen sie zuerst die Person auszuwählen")
 
 
     
