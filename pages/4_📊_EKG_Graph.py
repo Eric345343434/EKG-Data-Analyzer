1290c07f00ae1073d6d434e81065ccedf497a774
Eric345343434 <je3301@mci4me.at>
2024-07-09 18:39:04 +0200

    try-except

--- pages/4_📊_EKG_Graph.py
+++ pages/4_📊_EKG_Graph.py
@@ -9,19 +9,19 @@ if 'logged_in' not in st.session_state or not st.session_state.logged_in:
     st.warning("Bitte loggen Sie sich zuerst ein!")
     st.stop()
 
-#try:
-ekg_data= ekgdata.load_ekg_table()
-ekg_dict = ekg_data.get(doc_id=str(st.session_state.current_ekg_id))
-st.write("Datum des Ekg-Tests =",ekg_dict["date"])
+try:
+    ekg_data= ekgdata.load_ekg_table()
+    ekg_dict = ekg_data.get(doc_id=str(st.session_state.current_ekg_id))
+    st.write("Datum des Ekg-Tests =",ekg_dict["date"])
 
-ekgdata1= ekgdata(st.session_state.current_ekg_id)
+    ekgdata1= ekgdata(st.session_state.current_ekg_id)
 
-user_input_ekg_start= int(st.slider("Geben sie den Start Wert des Plots an",0, len(ekgdata1.df["Time in ms"]),0,))
-user_input_ekg_end= int(st.slider("Geben sie den End Wert des Plots an",0, len(ekgdata1.df["Time in ms"]),10000))
+    user_input_ekg_start= int(st.slider("Geben sie den Start Wert des Plots an",0, len(ekgdata1.df["Time in ms"]),0,))
+    user_input_ekg_end= int(st.slider("Geben sie den End Wert des Plots an",0, len(ekgdata1.df["Time in ms"]),10000))
 
-    # Plot EKG data with peaks
-st.plotly_chart(ekgdata1.plot_ekg_with_peaks(user_input_ekg_start,user_input_ekg_end))
-st.plotly_chart(ekgdata1.plot_ekg_with_peaks_hr(user_input_ekg_start,user_input_ekg_end))
-#except:
-    #st.title("EKG-Graph konnte nicht geladen werden!")
-    #st.header("Versuchen sie zuerst die Person und die Ekg-ID auszuwählen")
+        # Plot EKG data with peaks
+    st.plotly_chart(ekgdata1.plot_ekg_with_peaks(user_input_ekg_start,user_input_ekg_end))
+    st.plotly_chart(ekgdata1.plot_ekg_with_peaks_hr(user_input_ekg_start,user_input_ekg_end))
+except:
+    st.title("EKG-Graph konnte nicht geladen werden!")
+    st.header("Versuchen sie zuerst die Person und die Ekg-ID auszuwählen")
